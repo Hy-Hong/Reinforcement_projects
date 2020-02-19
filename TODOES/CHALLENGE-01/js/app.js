7bd3d2200f68b56b14867da481535e0f0d4d7845
hy.hong <hy.hong@PNC.LAN>
2020-02-19 20:54:52 +0700

    upload my miterm

--- TODOES/CHALLENGE-01/js/app.js
+++
@@ -1,9 +0,0 @@
-const dateElement = document.querySelector('#date');
-
-const option = {
-    weekday: 'long',
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric'
-};
-dateElement.innerHTML = new Date().toLocaleDateString('en-US', option);
